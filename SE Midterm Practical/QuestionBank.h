10348e06efba073ef75897735353c41ea1af2ae4
dg2022 <DCGarza@student.fullsail.edu>
2022-07-15 13:49:53 -0400

    Began wirk on Question 2

--- SE Midterm Practical/QuestionBank.h
+++ SE Midterm Practical/QuestionBank.h
@@ -7,10 +7,12 @@ class QuestionBank
 {
 
 public:
-	string Q1 = "What is the capital of Texas?";
+	string Q1 = "Question 1: What is the capital of Texas?";
 	bool Q1Answers();
 
-	string Q2;
+	string Q2 = "Question 2: Who was the Folk Hero that is famous for wearing a Racoon Skin Cap?";
+	bool Q2Answers();
+
 	string Q3;
 	string Q4;
 
--- SE Midterm Practical/SE Midterm Practical.cpp
+++ SE Midterm Practical/SE Midterm Practical.cpp
@@ -12,10 +12,17 @@
 
 int main()
 {
-    
+    int total;
     QuestionBank qbank;
+
+
     std::cout << qbank.Q1 << "\n";
     bool ans = qbank.Q1Answers();
+    if (ans = true)
+    {total++;}
+
+    std::cout << qbank.Q2 << "\n";
+
 
 }
 
